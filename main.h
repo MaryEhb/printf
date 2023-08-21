6a8a7b36c1c497906355530becd8e43eeec550eb
Jo02001 <ahmdusef5050@gmail.com>
2023-08-21 06:48:25 -0700

    commiting main.h file

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef _PRINTF_
+#define _PRINTF_
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct specifiers - specifiers grouped with their functions
+ * @spec: specifier
+ * @spec_func: specifier function to print output and return len of printed
+ */
+
+struct specifier
+{
+        char spec;
+        int (*spec_func)();
+};
+
+typedef struct specifier specifier;
+
+int _putchar(char c);
+int print_char(va_list args);
+int _printf(const char *format, ...);
+int print_digits(va_list args);
+int print_string(va_list args);
+
+#endif
