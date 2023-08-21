ed28953f774b7d145ab8dcb00e20ba5fb178b5ba
Jo02001 <ahmdusef5050@gmail.com>
2023-08-21 06:53:38 -0700

    commiting print_string.c file

---
+++ print_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_string - Prints a string and returns its length.
+ * @args: A va_list containing the arguments.
+ * Return: The length of the printed string.
+ */
+
+int print_string(va_list args)
+{
+        unsigned int len = 0;
+        char *s = va_arg(args, char *);
+
+        while (s && s[len])
+        {
+                _putchar(s[len]);
+                len++;
+        }
+        return (len);
+}
