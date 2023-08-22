ab24a832ffccd49b9a50690fef8a65f38fd77663
Mariem <mariem.ehab@ieee.org>
2023-08-22 05:18:27 -0700

    making sure its counting neg sign

--- print_digits.c
+++ print_digits.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdbool.h>
 /**
  * print_digits - Placeholder for printing a decimal (integer).
  * @args: A va_list containing the arguments.
@@ -9,11 +10,13 @@ int print_digits(va_list args)
 {
 	int numDigits = 1, temp, i = 1, divisor = 1;
 	int n = va_arg(args, int);
+	bool isneg = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
+		isneg = 1;
 	}
 
 	temp = n;
@@ -35,5 +38,5 @@ int print_digits(va_list args)
 		n %=  divisor;
 		divisor /= 10;
 	}
-	return (numDigits);
+	return (!isneg ? numDigits : numDigits + 1);
 }
